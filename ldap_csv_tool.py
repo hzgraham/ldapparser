3fc5bd56a66ffb8ad351b3f3bf35d0664f001386
Henry Graham <hgraham@redhat.com>
2016-09-14 23:30:50 -0400

    adding more to the LDAP connection

--- ldap_csv_tool.py
+++ ldap_csv_tool.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env pythonf
 
 import ldap, argparse
+from ldap import sasl, modlist
 
 
-def ldapQuery():
+def ldapQuery(self,args):
     ldap_host = ""
     ldap_base = ""
     ldap_entry = ""
     team = []
     l = ldap.initialize(ldap_host)
-    result = l.search_s(ldap_base,ldap.SCOPE_SUBTREE,ldap_entry)
+
+    try:
+        self.basedn = args.basedn
+        self.cacert = args.cacert
+        self.look_up = args.lookup_host
+    except:
+        print "Arguments need to be provided"
+    # Authenticated connection created
+    self.auth = ldap.sasl.gssapi("")
+
+    if self.ldap_host.startswith('ldap://'):
+        self.ldap_connection = ldap.initialize(self.ldap_host)
+    else:
+        self.ldap_connection = ldap.initialize("ldap://" + self.ldap_host)
+
+    print "GSSAPI bind happening"
+    self.ldap_connection.sasl_interactive_bind_s("", self.auth)
+
+    self.result = self.ldap_connection.search_s( self.ldap_base_dn, ldap.SCOPE_SUBTREE, self.search_entry)
+    print self.result
+    #result = l.search_s(ldap_base,ldap.SCOPE_SUBTREE,ldap_entry)
 
 def parseCSV(filename):
     filename = args.filename
@@ -24,17 +45,17 @@ if __name__ == "__main__":
                         help='The .csv file containing hosts and exclusions')
 
     parser.add_argument('--ldap-host','-m',
-                        #required=True,
+                        required=True,
                         dest='lookup_host',
                         help='enter the LDAP source host uri for example: ldap.example.com')
 
     parser.add_argument('--basedn','-b',
-                        #required=True,
+                        required=True,
                         dest='basedn',
                         help='The search base for LDAP for example: dc=example,dc=com')
 
     parser.add_argument('--cacert','-c',
-                        #required=True,
+                        required=True,
                         dest='cacert',
                         help='The path to the CA cert file or directory containing it,  if a directory then make sure it is managed by the c_rehash utility')
 
@@ -45,6 +66,6 @@ if __name__ == "__main__":
                         action=PromptAction, \
                         help='Plain text password for user')
 
+    args = parser.parse_args()
 
-
-
+    ldapQuery(args)
